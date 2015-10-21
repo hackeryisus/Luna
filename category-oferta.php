f25e26aaf2fa022b686c7e6a4ecdd716cbfa76af
jemandez <jeman.400@gmail.com>
2015-10-21 02:24:15 -0500

    Estilos Cursos complementarios Se arreglaron los estilos para los cursos complementarios -Se agregan los objetos de cursos -Estilos para los nuevos objetos

--- category-oferta.php
+++ category-oferta.php
@@ -12,29 +12,6 @@
 </div>
 </div>
 
-<div class="container">
-	<div class="row">
-		<div class="col-sm-12">
-			<div class="enigma_heading_title">
-				<h3>Cursos</h3>
-			</div>
-		</div>
-	</div>
-</div>
-<div class="container">
-<div class="row enigma_blog_wrapper">
-	<div class="col-md-8">
-		<?php query_posts('cat=5&order=Asc'); ?>
-		<?php if ( have_posts() ) :
-			while ( have_posts() ) : the_post(); get_template_part('post','cursos'); ?>
-		 <?php endwhile; ?>
-		 <!-- post navigation -->
-		 <?php else: ?>
-				<h4>No hemos encontrado resultados</h4>
-		 <!-- no posts found -->
-		 <?php endif; ?>
-	</div>
-</div>
-</div>
+<?php get_template_part('cursos'); ?>
 <?php get_template_part('certificadores'); ?>
 <?php get_footer(); ?>
---
+++ cursos.php
@@ -0,0 +1,26 @@
+<div class="container">
+	<div class="row">
+		<div class="col-sm-12">
+			<div class="enigma_heading_title">
+				<h3>Cursos Complementarios</h3>
+			</div>
+		</div>
+	</div>
+  <div class="row ">
+  		<?php query_posts('cat=6&order=Asc'); ?>
+  		<?php if ( have_posts() ) :
+  			while ( have_posts() ) : the_post(); ?>
+        <article class="col-sm-4 cursos">
+          <h3><?php if(!is_single()) {?><a href="<?php the_permalink(); ?>"><?php } ?><?php the_title(); ?></a></h3>
+          <p><?php the_excerpt(); ?></p>
+        </article>
+         <!--get_template_part('post','cursos');-->
+  		 <?php endwhile; ?>
+  		 <!-- post navigation -->
+  		 <?php else: ?>
+  				<h4>No hemos encontrado resultados</h4>
+  		 <!-- no posts found -->
+  		 <?php endif; ?>
+  </div>
+</div>
+<br>
