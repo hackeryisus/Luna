a4324fd46cff36377c464d41ae136c0ddc1169d5
jemandez <jeman.400@gmail.com>
2015-10-22 06:11:51 -0500

    Se elimina la sidebar de todas las paginas

--- single.php
+++ single.php
@@ -1,19 +1,18 @@
-<?php get_header(); 
+<?php get_header();
 get_template_part('breadcrums'); ?>
-<div class="container">	
+<div class="container">
 	<div class="row enigma_blog_wrapper">
-	<div class="col-md-8">	
-		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>		
-		<?php get_template_part('post','content'); 
+	<div class="col-md-8">
+		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+		<?php get_template_part('post','content');
 		get_template_part('author','intro');
-		endwhile; 
-		else : 
+		endwhile;
+		else :
 		get_template_part('nocontent');
 		endif;
 		weblizar_navigation_posts();
 		comments_template( '', true ); ?>
 	</div>
-	<?php get_sidebar(); ?>	
 	</div> <!-- row div end here -->	
 </div><!-- container div end here -->
 <?php get_footer(); ?>
